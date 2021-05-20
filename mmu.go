7de0450f21cb2be01cd4e878a86e6e54358d2250
Mark Davidson <mark@bytehigh.com>
2021-05-20 23:53:03 +0100

    Handle boot rom disable

--- mmu.go
+++ mmu.go
@@ -16,4 +16,17 @@ func (gbmmu *mmu) fetchByte(address uint16) byte {
 func (gbmmu *mmu) storeByte(address uint16, value byte) {
 	tstates += 4
 	gbmmu.memory[address] = value
+
+	switch address {
+	case 0xFF50:
+		// if DMG rom is turned off, copy the first 256 bytes of the ROM into memory
+		if value > 0 {
+			bootpage := getBootpage()
+			for i, opcode := range bootpage {
+				gbmmu.memory[i] = opcode
+			}
+		}
+	default:
+
+	}
 }
